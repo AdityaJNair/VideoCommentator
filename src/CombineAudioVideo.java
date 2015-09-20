bba063c14688fc1a8388898072f6527b3472f5d6
PriyankitSingh <psin778@aucklanduni.ac.nz>
2015-09-20 15:36:32 +1200

    Added SA to ffmpeg class

--- src/CombineAudioVideo.java
+++ src/CombineAudioVideo.java
@@ -16,6 +16,7 @@ public class CombineAudioVideo extends SwingWorker<Void, Void> {
 	private String audioFileName;
 	private String videoFileName;
 	private JDialog dialog;
+	private String outputPath;
 
 	/**
 	 * Creates a new CombineAudioVideo object
@@ -31,9 +32,11 @@ public class CombineAudioVideo extends SwingWorker<Void, Void> {
 
 	@Override
 	protected Void doInBackground() throws Exception {
+		SaveAs sa = new SaveAs();
+		String path = sa.getSelectionPath();
 		createDialog();
 		cmd = "ffmpeg -i " + videoFileName + " -i " + audioFileName
-				+ " -filter_complex amix=inputs=2 out.mp4";
+				+ " -filter_complex amix=inputs=2 " + path;
 		System.out.println(cmd);
 		builder = new ProcessBuilder("/bin/bash", "-c", cmd);
 		try {
