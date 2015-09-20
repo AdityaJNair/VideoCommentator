0ef2b16775461b872d9a27098b456742b6da48f9
PriyankitSingh <psin778@aucklanduni.ac.nz>
2015-09-20 15:28:21 +1200

    Save as

--- src/SaveAs.java
+++ src/SaveAs.java
@@ -3,17 +3,48 @@ import java.io.File;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 
+/** 
+ * SaveAs dialog box lets users choose the filename of output file.
+ * Used to save mp4 files.
+ * @author Adi Nair, Priyankit Singh
+ *
+ */
+public class SaveAs extends JFrame {
 
-public class SaveAs extends JFrame{
+	JFileChooser fileChooser;
 
-	SaveAs(){
-		JFileChooser fileChooser = new JFileChooser();
+	/**
+	 * Creates a new Save As dialog box.
+	 */
+	SaveAs() {
+		fileChooser = new JFileChooser();
 		fileChooser.setDialogTitle("Select output File Location");
+	}
 
+	/**
+	 * Gets the selection path for output files from users.
+	 * @return Returns the path to the location selected by the user. Null if no file selected
+	 */
+	public String getSelectionPath() {
 		int userSelection = fileChooser.showSaveDialog(this);
 		if (userSelection == JFileChooser.APPROVE_OPTION) {
 			File fileToSave = fileChooser.getSelectedFile();
-			System.out.println("Save as file: " + fileToSave.getAbsolutePath());
+			File f = new File(fileToSave.getAbsolutePath() + ".mp4");
+			if (f.exists()) {
+				int count = 0;
+				while (f.exists()) {
+					count++;
+					f = new File(fileToSave.getAbsolutePath() + "(" + count
+							+ ")" + ".mp4");
+				}
+				return fileToSave.getAbsolutePath() + "(" + count + ")"
+						+ ".mp4";
+			} else {
+				return fileToSave.getAbsolutePath() + ".mp4";
+			}
+
 		}
+		return null;
 	}
+
 }
